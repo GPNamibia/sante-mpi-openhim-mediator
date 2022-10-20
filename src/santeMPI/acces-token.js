fee0fd58cf498054d92dce04ee6330379649787d
Collin Nehemia <collin.nehemia@ucglobalprograms.org>
2022-10-20 08:49:07 +0200

    NAUPID-93(Extracted the correct user auth headers for password and client_credentials grant_type)

--- src/santeMPI/acces-token.js
+++ src/santeMPI/acces-token.js
@@ -6,40 +6,45 @@ const APP_SECRET = privateConfig.santeMpiConfig.client_secret;
 const { SanteAPI } = require("./sante-mpi-api");
 const santeAPI = new SanteAPI();
 
-const getAccessTokenPasswordGrant = async (username, password) => {
+const getAccessTokenPasswordGrant = async (username, password, client_id, client_secret, grant_type) => {
     return new Promise(async (resolve, reject) => {
-        const data = {
-            grant_type: privateConfig.santeMpiConfig.grant_type_p,
-            client_id: CLIENT_ID,
-            client_secret: APP_SECRET,
-            username: username,
-            password: password
-        };
-        //call get token api
-        await santeAPI.getTokenPassword(data).then((res) => {
-            console.log("Bearer "+res.access_token)
-            return resolve(res);
-        }).catch(error=>{
+        if (username, password, client_id, client_secret, grant_type) {
+            const data = {
+                grant_type: grant_type,
+                client_id: client_id,
+                client_secret: client_secret,
+                username: username,
+                password: password
+            };
+            //call get token api
+            await santeAPI.getTokenPassword(data).then((res) => {
+                return resolve(res);
+            }).catch(error => {
+                return resolve(error);
+            })
+        } else {
             return resolve(error);
-        })
+        }
     })
 };
 
-const getAccessTokenCredentialGrant = async (username, password) => {
+const getAccessTokenCredentialGrant = async (client_id, client_secret, grant_type) => {
     return new Promise(async (resolve, reject) => {
-        const data = {
-            grant_type: privateConfig.santeMpiConfig.grant_type,
-            client_id: CLIENT_ID,
-            client_secret: APP_SECRET,
-            username: username,
-            password: password
-        };
-        //call get token api
-        await santeAPI.getTokenPassword(data).then((res) => {
-            return resolve("Bearer " + res.access_token);
-        }).catch(error=>{
+        if (client_id, client_secret, grant_type) {
+            const data = {
+                grant_type: grant_type,
+                client_id: client_id,
+                client_secret: client_secret,
+            };
+            //call get token api
+            await santeAPI.getTokenPassword(data).then((res) => {
+                return resolve(res);
+            }).catch(error => {
+                return resolve(error);
+            })
+        } else {
             return resolve(error);
-        })
+        }
     })
 };
 
